583d422db1e9cd651b0d135fb01bd91d0dd9f46c
Matthew Wimberly <mawimberly@csumb.edu>
2018-07-25 20:29:20 -0700

    Added error partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-
   def show
     @user = User.find(params[:id])
   end
@@ -7,4 +6,19 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # Save
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permite(:name, :email, :password, :password_confirmation)
+    end
 end
