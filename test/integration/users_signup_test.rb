8dbcd86e1d9a47716849206b29199c521423a88b
Matthew Wimberly <mawimberly@csumb.edu>
2018-07-27 09:49:33 -0700

    Added all tests for user signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -7,5 +7,39 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       post users_path, params: { user: { name: '', email: 'user@invalid', password: 'foo', password_confirmation: 'bar' } }
     end
     assert_template 'users/new'
+    assert_select 'div#<CSS id for error explanation>'
+    assert_select 'div.<CSS class for field with error>'
+  end
+
+  test "invalid signup information - Name" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name: '', email: 'user@invalid.com', password: 'foo', password_confirmation: 'foo' } }
+    end
+    assert_template 'users/new'
+  end
+
+  test "invalid signup information - Email" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name: 'Matthew', email: 'user@invalidcom', password: 'foo', password_confirmation: 'foo' } }
+    end
+    assert_template 'users/new'
+  end
+
+  test "invalid signup information - Password" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name: 'Matthew', email: 'user@invalid.com', password: 'bar', password_confirmation: 'foo' } }
+    end
+    assert_template 'users/new'
+  end
+
+  test "invalid signup information - Password Confirmation" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name: 'Matthew', email: 'user@invalid.com', password: 'bar', password_confirmation: 'Bar' } }
+    end
+    assert_template 'users/new'
   end
 end
