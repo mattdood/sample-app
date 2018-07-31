100d36f0501693bdb8662347306bb4f0872f42e0
Matthew Wimberly <mawimberly@csumb.edu>
2018-07-31 10:02:18 -0700

    Added user signup tests

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -7,8 +7,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       post users_path, params: { user: { name: '', email: 'user@invalid', password: 'foo', password_confirmation: 'bar' } }
     end
     assert_template 'users/new'
-    assert_select 'div#form[action="/signup"]'
-    assert_select 'div#form[action="/signup"]'
   end
 
   test "invalid signup information - Name" do
@@ -42,4 +40,24 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     assert_template 'users/new'
   end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name: 'Mattdood', email: 'example@valid.com', password: 'foobar', password_confirmation: 'foobar' } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+  end
+
+  test "valid flash after signup" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name: 'Mattdood', email: 'example@valid.com', password: 'foobar', password_confirmation: 'foobar' } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
+  end
+
 end
