c9a95db7860ad64fe48076477d869a16386b20c2
Matthew Wimberly <mawimberly@csumb.edu>
2018-08-07 10:16:25 -0700

    Added remember me integration tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,4 @@
+
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
@@ -7,8 +8,20 @@ class ActiveSupport::TestCase
   fixtures :all
   include ApplicationHelper
 
-  def  is_logged_in?
+  def is_logged_in?
     !session[:user_id].nil?
   end
-  # Add more helper methods to be used by all tests here...
+
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
+
+end
+
+class ActionDispatch::IntegrationTest
+
+  def log_in_as(user, password: 'password', remember_me: '1')
+    post login_path, params: { session: { email: user.email, password: password, remember_me: remember_me } }
+  end
+  
 end
