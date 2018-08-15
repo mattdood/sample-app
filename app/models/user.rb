70b146a4574334640449e9bfe10c9041c63417e2
Matthew Wimberly <mawimberly@csumb.edu>
2018-08-15 14:27:50 -0700

    Added create action and integrated with user accessor

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
 
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token, :reset_token
 
   before_save { email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }
