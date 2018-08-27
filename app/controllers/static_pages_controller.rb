cdadb53eeb83e9c6d446a92b696d102026863bc1
Matthew Wimberly <mawimberly@csumb.edu>
2018-08-27 11:58:28 -0700

    Form partials added for microposts on home page, code refactored on static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,7 @@
 class StaticPagesController < ApplicationController
+
   def home
+    @micropost = current_user.microposts.build if logged_in?
   end
 
   def help
@@ -16,5 +18,5 @@ class StaticPagesController < ApplicationController
 
   def login
   end
-  
+
 end
