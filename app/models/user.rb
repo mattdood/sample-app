1796dfd4e9147207cc678649507a16351e723e57
Matthew Wimberly <mawimberly@csumb.edu>
2018-09-17 10:14:32 -0700

    Added user.rb feed for following users

--- app/models/user.rb
+++ app/models/user.rb
@@ -63,7 +63,8 @@ class User < ApplicationRecord
   end
 
   def feed
-    Micropost.where("user_id = ?", id)
+    following_ids = "SELECT followed_ID FROM relationships WHERE follower_id = :user_id"
+    Micropost.where("user_id = IN (#{following_ids}) OR user_id = :user_id", user_id: id)
   end
 
   # Follower functions
